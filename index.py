680414f783ed550dcbcafccd67fd3dca663ac759
Daryl <slyr91@gmail.com>
2023-07-21 07:27:28 -0700

    Template Ready Template file is ready. Next is to build the logic behind it and get the weather information for the location.

---
+++ index.py
@@ -0,0 +1,10 @@
+from flask import (
+    Blueprint, render_template
+)
+
+bp = Blueprint('index', __name__)
+
+
+@bp.route('/', methods=['GET'])
+def index():
+    return render_template('app/index.html')

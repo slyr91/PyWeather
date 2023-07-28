cca6b89c4a2d0be90ca308d97f5f031753c14152
Daryl <slyr91@gmail.com>
2023-07-27 23:32:11 -0700

    Modified Procfile I am trying to get the worker to start up. I haven't done this with the main class so I am not sure if this will work.

--- main.py
+++ main.py
@@ -17,4 +17,3 @@ if __name__ == '__main__':
     host = '127.0.0.1' if port == 8080 else '0.0.0.0'
     app.run(host=host, port=port, debug=False)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
